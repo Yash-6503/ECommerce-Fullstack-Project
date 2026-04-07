714702ba821416e60e7c05975e88ef256abc2277
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2026-04-07 18:55:15 +0530

    User Repository

---
+++ ecommerce-backend/src/main/java/com/ecom/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecom.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecom.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
+}
