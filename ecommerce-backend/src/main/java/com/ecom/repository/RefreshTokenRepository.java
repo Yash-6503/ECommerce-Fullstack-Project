2ab7d33ac5fe77aaa61704030321b818b83c6d49
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2026-04-08 19:37:12 +0530

    Added Refresh Token Repository Interface

---
+++ ecommerce-backend/src/main/java/com/ecom/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.ecom.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecom.entity.RefreshToken;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    void delete(RefreshToken token);
+}
