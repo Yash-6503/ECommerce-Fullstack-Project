783574e8781cec2b69f3ebb1a162c2c59cb9e1b0
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2026-04-08 19:40:02 +0530

    Added login, logout and refresh

--- ecommerce-backend/src/main/java/com/ecom/service/AuthService.java
+++ ecommerce-backend/src/main/java/com/ecom/service/AuthService.java
@@ -1,5 +1,8 @@
 package com.ecom.service;
 
+import com.ecom.dto.AuthResponse;
+import com.ecom.dto.LoginRequest;
+import com.ecom.dto.RefreshTokenRequest;
 import com.ecom.dto.RegisterRequest;
 
 public interface AuthService {
@@ -7,5 +10,7 @@ public interface AuthService {
 	public String sendOtp(String identifier);
 	public String verifyOtp(String identifier, String otpInput);
 	public String register(RegisterRequest request);
-	
+	public AuthResponse login(LoginRequest request);
+	public AuthResponse refreshToken(RefreshTokenRequest refreshToken);
+	public String logout(String username);
 }
