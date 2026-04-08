76c3755426e858a6a9622f30ae600782b493bfed
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2026-04-08 19:33:10 +0530

    Renamed User class to Customer Class

---
+++ ecommerce-backend/src/main/java/com/ecom/entity/Customer.java
@@ -0,0 +1,69 @@
+package com.ecom.entity;
+
+import com.ecom.enums.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String phone;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    // getters & setters
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+    
+    
+    
+}
