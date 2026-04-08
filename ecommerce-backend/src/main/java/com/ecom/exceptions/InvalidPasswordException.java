d00769859f1949e30beb7811c28b2af02e48fd52
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2026-04-08 19:35:49 +0530

    Invalid Password Exception class

---
+++ ecommerce-backend/src/main/java/com/ecom/exceptions/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package com.ecom.exceptions;
+
+public class InvalidPasswordException extends RuntimeException {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
